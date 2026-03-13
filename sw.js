efbe0e6a142f48c458ca036f1e9af509929b4cde
pozopearce-cloud <pozo.pearce@gmail.com>
2026-03-13 04:32:48 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,16 +8,12 @@ const ASSETS = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
