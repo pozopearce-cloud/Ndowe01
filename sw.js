6fe282e98090c51fe57948e2c6c696c9a8d4a069
pozopearce-cloud <pozo.pearce@gmail.com>
2026-03-13 02:17:37 +0100

    sw.js
    Service Worker

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'scango-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  '[https://cdn.tailwindcss.com](https://cdn.tailwindcss.com)'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
